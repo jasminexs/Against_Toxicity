813cc36d466f8c79c8bb509daf79a101024c92e8
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-09-11 21:24:48 -0400

    precompile regex patterns

--- src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/NameHelper.java
+++ src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/NameHelper.java
@@ -6,24 +6,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NameHelper {
+    private static final Pattern PREFIX_PATTERN = Pattern.compile("(<)?(BOOSTER |MOD |ADMIN |DEV |YOUTUBE |Stray |VIP|<-- |🌙 |☽ |❤ |⚡ |☠ |✟ |⚔ |⚒ |☀ |Party]|\\[)?([^>\\s:]+)");
+    private static final Pattern BLOWN_UP_PATTERN = Pattern.compile("was blown up by (\\w+)");
+    private static final Pattern SLAIN_PATTERN = Pattern.compile("was slain by (\\w+)");
+
     static ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
     public static String getUsername(String input) {
-        Pattern pattern1 = Pattern.compile("(<)?(BOOSTER |MOD |ADMIN |DEV |YOUTUBE |Stray |VIP|<-- |🌙 |☽ |❤ |⚡ |☠ |✟ |⚔ |⚒ |☀ |Party]|\\[)?([^>\\s:]+)");
-        Matcher matcher1 = pattern1.matcher(input);
+        Matcher matcher1 = PREFIX_PATTERN.matcher(input);
         boolean ignorePlayer = false;
         String[] ignoreNames = Lists.ignoreNames;
         String[] friends = config.getFriends();
 
         if (input.contains("was blown up by")) {
-            Pattern pattern2 = Pattern.compile("was blown up by (\\w+)");
-            Matcher matcher2 = pattern2.matcher(input.substring(input.indexOf(" was ")));
+            Matcher matcher2 = BLOWN_UP_PATTERN.matcher(input.substring(input.indexOf(" was ")));
             if (config.isDebug()) System.out.println("[AgainstToxicity] NameHelper - player was blown up");
             if (matcher2.find()) return matcher2.group(1);
         }
 
         if (input.contains("was slain by")) {
-            Pattern pattern3 = Pattern.compile("was slain by (\\w+)");
-            Matcher matcher3 = pattern3.matcher(input.substring(input.indexOf(" was ")));
+            Matcher matcher3 = SLAIN_PATTERN.matcher(input.substring(input.indexOf(" was ")));
             if (config.isDebug()) System.out.println("[AgainstToxicity] NameHelper - player was slain");
             if (matcher3.find()) return matcher3.group(1);
         }
