e29a672bc5b0f69982a66205638727cfe9a916c7
epicgamerjamer <pajamerz041@gmail.com>
2023-10-02 00:18:22 -0400

    private handling is done in MixinChat (removed boolean p)

--- src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/TextBuilder.java
+++ src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/TextBuilder.java
@@ -6,11 +6,9 @@ public class TextBuilder {
     private static final Random random = new Random();
     private final String name;
     private final String response;
-    private final boolean priv;
 
-    public TextBuilder(String n, int l, boolean p) {
+    public TextBuilder(String n, int l) {
         name = n;
-        priv = p;
         if (l == 1) {
             response = Lists.AntiToxicList[random.nextInt(Lists.AntiToxicList.length)];
         } else if (l == 2) {
@@ -19,13 +17,7 @@ public class TextBuilder {
             response = null;
         }
     }
-
     public String toString() {
-        if (priv) {
-            return("msg " + name + " " + name + response);
-        }
-        else {
             return (name + response);
-        }
     }
 }
