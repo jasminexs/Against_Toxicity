64afb08fcba392fd3eff4cb6f754ef018708e6af
epicgamerjamer <pajamerz041@gmail.com>
2023-10-02 00:17:33 -0400

    better protection against server kicking you for attempting to send "§" and general fixes

--- src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/NameHelper.java
+++ src/main/java/net/epicgamerjamer/mod/againsttoxicity/client/NameHelper.java
@@ -1,30 +1,55 @@
 package net.epicgamerjamer.mod.againsttoxicity.client;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import org.spongepowered.asm.mixin.Unique;
+
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NameHelper {
-
     //    Thanks to Crosby/RacoonDog for optimizing regex
-    private static final Pattern prefixes = Pattern.compile("(<)?(BOOSTER |MOD |ADMIN |DEV |YOUTUBE |Stray |VIP|<-- |🌙 |☽ |❤ |⚡ |☠ |✟ |⚔ |⚒ |☀ |⭐ |Party]|\\[)?([^>\\s:]+)");
+    private static final Pattern prefixes = Pattern.compile("(<)?(BOOSTER |MOD |ADMIN |DEV |YOUTUBE |Stray |VIP|<-- |Party]|Dead]|\\[)?([^>\\s:]+)");
     private static final Pattern blownup = Pattern.compile("was blown up by (\\w+)");
     private static final Pattern slain = Pattern.compile("was slain by (\\w+)");
-
+    @Unique
+    private static ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    @Unique
+    private static String[] friends = config.friends;
     public static String getUsername(String input) {
+        if (config.debug) System.out.println(Arrays.toString(friends));
+        input = input.replace("\\", "")
+                .replace("/", "")
+                .replace("[", "")
+                .replace("]", "")
+                .replace("{", "")
+                .replace("}", "")
+                .replace("(", "")
+                .replace(")", "")
+                .replace("?", "")
+                .replace("!", "")
+                .replace("*", "")
+                .replace(".", "")
+                .replace(";", "")
+                .replace(":", "")
+                .replace("'", "")
+                .replace("\"", "")
+                .replace("|", "")
+                .replace("@", "")
+                .replace(",", "")
+                .replace(".", "")
+                .replace("$", "");
         Matcher matcher1 = prefixes.matcher(input);
         boolean ignorePlayer = false;
         String[] ignoreNames = Lists.IgnoreNames;
-        String[] friends = ModConfig.friends;
 
         if (input.contains("was blown up by")) {
             Matcher matcher2 = blownup.matcher(input.substring(input.indexOf(" was ")));
-            if (ModConfig.debug) System.out.println("[AgainstToxicity] NameHelper - player was blown up");
             if (matcher2.find()) return matcher2.group(1);
         }
 
         if (input.contains("was slain by")) {
             Matcher matcher3 = slain.matcher(input.substring(input.indexOf(" was ")));
-            if (ModConfig.debug) System.out.println("[AgainstToxicity] NameHelper - player was slain");
             if (matcher3.find()) return matcher3.group(1);
         }
 
@@ -44,11 +69,12 @@ public class NameHelper {
             if (!ignorePlayer) {
                 String name = matcher1.group(3);
                 if (matcher1.group(3).contains("--")) name = matcher1.group(3).substring(2);
-                return name;
+                if (name.length() == 1) {
+                    return getUsername(input.substring(1).replace("§",""));
+                }
+                return name.replace("§","");
             }
         }
-
-
         return null;
     }
 }
