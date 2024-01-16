583df81dfd73328687654f2aeb391f0bf72d5fc9
epicgamerjamer <pajamerz041@gmail.com>
2024-01-15 19:33:27 -0500

    now works for messaging players and doesn't run when mod is disabled

--- src/main/java/net/epicgamerjamer/mod/againsttoxicity/mixin/ChatScreenMixin.java
+++ src/main/java/net/epicgamerjamer/mod/againsttoxicity/mixin/ChatScreenMixin.java
@@ -1,21 +1,32 @@
 package net.epicgamerjamer.mod.againsttoxicity.mixin;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.epicgamerjamer.mod.againsttoxicity.client.ChatProcessor;
+import net.epicgamerjamer.mod.againsttoxicity.client.Config;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
+    @Unique
+    private static Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
 
     @Inject(method = "sendMessage", at = @At("HEAD"), cancellable = true)
     public void onSendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
-        if (new ChatProcessor(chatText.toLowerCase()).getToxicity() > 0 && !chatText.startsWith("/")) {
+        if (config.modEnabled && new ChatProcessor(chatText.toLowerCase()).getToxicity() > 0 && (
+                chatText.startsWith("/r ") ||
+                chatText.startsWith("/msg ") ||
+                chatText.startsWith("/w ") ||
+                chatText.startsWith("/tell ") ||
+                !chatText.startsWith("/"))
+        ) {
             MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(
                     Text.literal("Try rewriting that to be less toxic.").formatted(Formatting.GRAY)
             );
