69f6f90b9d48256c6e78f795b278a666519b514d
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-08-27 20:54:39 -0400

    fix 4 1.21

--- src/main/java/net/epicgamerjamer/mod/againsttoxicity/mixin/ChatScreenMixin.java
+++ src/main/java/net/epicgamerjamer/mod/againsttoxicity/mixin/ChatScreenMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
@@ -18,8 +19,8 @@ public class ChatScreenMixin {
     @Unique
     private static Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
 
-    @Inject(method = "sendMessage", at = @At("HEAD"), cancellable = true)
-    public void onSendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "sendMessage(Ljava/lang/String;Z)V", at = @At("HEAD"), cancellable = true)
+    public void onSendMessage(String chatText, boolean addToHistory, CallbackInfo ci) {
         if (config.modEnabled && new ChatProcessor(chatText.toLowerCase()).getToxicity() > 0 && (
                 chatText.startsWith("/r ") ||
                 chatText.startsWith("/msg ") ||
@@ -30,7 +31,7 @@ public class ChatScreenMixin {
             MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(
                     Text.literal("Try rewriting that to be less toxic.").formatted(Formatting.GRAY)
             );
-            cir.setReturnValue(false);
+            ci.cancel();
         }
     }
 }
